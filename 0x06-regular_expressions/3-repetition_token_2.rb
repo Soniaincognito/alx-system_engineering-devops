121bcc4e7c332c53d7b239ebabe8d2c851b42b8a
Soniaincognito <opone345@gmail.com>
2024-09-13 03:31:43 -0700

    3-repetition_token_2.rb

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,6 +1,21 @@
- #!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression,
-# matching method
-# The regular expression must match the given cases
+#!/usr/bin/env ruby
 
-puts ARGV[0].scan(/hbt+n/).join
+# Regular expression pattern
+pattern = /^(aa|bb|cc|dd|ee|ff|gg|hh|ii|jj|kk|ll|mm|nn|oo|pp|qq|rr|ss|tt|uu|vv|ww|xx|yy|zz)$/
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: ruby 3-repetition_token_2.rb <string>"
+  exit
+end
+
+# Get the input string
+input = ARGV[0]
+
+# Match the pattern
+if pattern.match?(input)
+  puts "Match"
+else
+  puts "No Match"
+end
+
